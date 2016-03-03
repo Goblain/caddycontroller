8fd4aa1b4b622544deda7261bd01eafc089f8e5c
Radek 'Goblin' Pieczonka <goblin@pentex.pl>
2016-03-03 16:52:46 +0000

    WIP

--- controller.go
+++ controller.go
@@ -13,6 +13,7 @@ import (
 //  "k8s.io/kubernetes/pkg/util"
 )
 
+// Keep a Caddyfile template in a constant
 const (
   templatesrc = `
 {{range $vhost,$paths := . }}
@@ -34,26 +35,26 @@ type VHost map[string]Endpoints
 type Router map[string]VHost
 
 func getRouter () Router {
+  kubeClient, err := client.NewInCluster()
+//  services, err := kubeClient.Services(api.NamespaceAll).List(api.ListOptions{})
+//  if err != nil { panic(err) }
+  endpoints, err := kubeClient.Endpoints(api.NamespaceAll).List(api.ListOptions{})
+  if err != nil { panic(err) }
+  fmt.Printf("%v \n", endpoints)
   router := make(Router)
   var ingressClient client.IngressInterface
-  kubeClient, err := client.NewInCluster()
   if err != nil { panic(err) }
   ingressClient = kubeClient.Extensions().Ingress(api.NamespaceAll)
   ingresses, err := ingressClient.List(api.ListOptions{})
   for _,ingress := range ingresses.Items {
-//    fmt.Printf("Ingress %v \n\n", ingress.Namespace)
     for _,rule := range ingress.Spec.Rules {
-//      fmt.Printf("Host: %v \n", rule.Host)
       _, ok := router[rule.Host]
       if ok==false { router[rule.Host] = make(VHost) }
       for _,path := range rule.IngressRuleValue.HTTP.Paths {
         router[rule.Host][path.Path] = make(Endpoints)
-        fmt.Printf("Path: %v \n", path.Path)
-        fmt.Printf("ServiceName: %v \n", path.Backend.ServiceName)
-        fmt.Printf("ServicePort: %v \n", path.Backend.ServicePort.IntVal)
-        services, err := kubeClient.Services(ingress.Namespace).List(api.ListOptions{})
-        if err != nil { panic(err) }
-        fmt.Printf("%v \n", services)
+//        fmt.Printf("Path: %v \n", path.Path)
+//        fmt.Printf("ServiceName: %v \n", path.Backend.ServiceName)
+//        fmt.Printf("ServicePort: %v \n", path.Backend.ServicePort.IntVal)
       }
     }
   }
