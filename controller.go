f835a46e6fd7aaf36da9cbf9612a8400281a6d62
Radek 'Goblin' Pieczonka <goblin@pentex.pl>
2016-03-04 14:13:12 +0000

    WIP

--- controller.go
+++ controller.go
@@ -2,24 +2,28 @@ package main
 
 import (
   "fmt"
-//  "log"
+  "strconv"
+  "bytes"
   "os"
-//  "os/exec"
-//  "reflect"
   "text/template"
   "k8s.io/kubernetes/pkg/api"
-//  "k8s.io/kubernetes/pkg/apis/extensions"
   client "k8s.io/kubernetes/pkg/client/unversioned"
-//  "k8s.io/kubernetes/pkg/util"
+  "github.com/mholt/caddy/caddy"
+)
+
+var (
+  AppName string = "CaddyIngress"
+  AppVersion string = "v0.1"
 )
 
 // Keep a Caddyfile template in a constant
 const (
+  DefaultConfigFile = "/Caddyfile"
   templatesrc = `
 {{range $vhost,$paths := . }}
 http://{{$vhost}} {
-{{range $path,$endpoints := $paths }}
-  proxy {{$path}} {{range $endpoint := $endpoints}}{{$endpoint}}{{end}} {
+{{range $path,$service := $paths }}
+  proxy {{$path}} {{$service}} {
     proxy_header X-Real-IP {remote}
   }
 {{end}}
@@ -28,42 +32,55 @@ http://{{$vhost}} {
 `
 )
 
-type Endpoints map[string]string
-
-type VHost map[string]Endpoints
+type VHost map[string]string
 
 type Router map[string]VHost
 
-func getRouter () Router {
-  kubeClient, err := client.NewInCluster()
-//  services, err := kubeClient.Services(api.NamespaceAll).List(api.ListOptions{})
-//  if err != nil { panic(err) }
-  endpoints, err := kubeClient.Endpoints(api.NamespaceAll).List(api.ListOptions{})
-  if err != nil { panic(err) }
-  fmt.Printf("%v \n", endpoints)
+func getRouter (kubeClient *client.Client) Router {
   router := make(Router)
-  var ingressClient client.IngressInterface
-  if err != nil { panic(err) }
-  ingressClient = kubeClient.Extensions().Ingress(api.NamespaceAll)
+  ingressClient := kubeClient.Extensions().Ingress(api.NamespaceAll)
   ingresses, err := ingressClient.List(api.ListOptions{})
+  if err != nil { panic(err) }
   for _,ingress := range ingresses.Items {
     for _,rule := range ingress.Spec.Rules {
       _, ok := router[rule.Host]
       if ok==false { router[rule.Host] = make(VHost) }
       for _,path := range rule.IngressRuleValue.HTTP.Paths {
-        router[rule.Host][path.Path] = make(Endpoints)
-//        fmt.Printf("Path: %v \n", path.Path)
-//        fmt.Printf("ServiceName: %v \n", path.Backend.ServiceName)
-//        fmt.Printf("ServicePort: %v \n", path.Backend.ServicePort.IntVal)
+        router[rule.Host][path.Path] = path.Backend.ServiceName+"."+ingress.Namespace+":"+strconv.Itoa(int(path.Backend.ServicePort.IntVal))
       }
     }
   }
   return router
 }
 
-func main() {
-  router := getRouter()
+func regenerateCaddyfile(router Router) []byte {
   tpl, err := template.New("test").Parse(templatesrc)
   if err != nil { panic(err) }
-  err = tpl.Execute(os.Stdout, router)
+  var buffer bytes.Buffer
+  err = tpl.Execute(&buffer, router)                                                                                                                                                                                                                            
+  if err != nil { panic(err) }
+  return buffer.Bytes()
+}
+
+func main() {
+  kubeClient, err := client.NewInCluster()
+  if err != nil { panic(err) }
+  router := getRouter(kubeClient)
+  watch, err := kubeClient.Extensions().Ingress(api.NamespaceAll).Watch(api.ListOptions{})
+  if err != nil { panic(err) }  
+  evts := watch.ResultChan()
+  for {
+    evt := <-evts
+    if caddy.IsRestart() {
+      fmt.Printf("Skip Caddy restart on evt : %v", evt)
+      os.Setenv("CADDY_RESTART", "false")
+    } else {
+      fmt.Printf("Restart Caddy due to evt : %v", evt)
+      err := caddy.Restart(caddy.CaddyfileInput{Contents: []byte(regenerateCaddyfile(router))})
+      if err != nil { panic(err) }
+//      caddy.Wait()
+      os.Exit(0)
+    }
+  }
+
 }
