704912cdd939b10bce19d688b1ece0912ff7a859
Radek 'Goblin' Pieczonka <goblin@pentex.pl>
2016-03-05 10:20:18 +0000

    refactor to manage external caddy process

--- controller.go
+++ controller.go
@@ -1,19 +1,16 @@
 package main
 
 import (
+  "syscall"
+  "time"
   "fmt"
+  "io/ioutil"
   "strconv"
   "bytes"
-  "os"
+  "os/exec"
   "text/template"
   "k8s.io/kubernetes/pkg/api"
   client "k8s.io/kubernetes/pkg/client/unversioned"
-  "github.com/mholt/caddy/caddy"
-)
-
-var (
-  AppName string = "CaddyIngress"
-  AppVersion string = "v0.1"
 )
 
 // Keep a Caddyfile template in a constant
@@ -21,7 +18,7 @@ const (
   DefaultConfigFile = "/Caddyfile"
   templatesrc = `
 {{range $vhost,$paths := . }}
-http://{{$vhost}} {
+http://{{$vhost}}:80 {
 {{range $path,$service := $paths }}
   proxy {{$path}} {{$service}} {
     proxy_header X-Real-IP {remote}
@@ -53,34 +50,53 @@ func getRouter (kubeClient *client.Client) Router {
   return router
 }
 
-func regenerateCaddyfile(router Router) []byte {
+func regenerateCaddyfile(router Router) {
   tpl, err := template.New("test").Parse(templatesrc)
   if err != nil { panic(err) }
   var buffer bytes.Buffer
-  err = tpl.Execute(&buffer, router)                                                                                                                                                                                                                            
+  err = tpl.Execute(&buffer, router)
   if err != nil { panic(err) }
-  return buffer.Bytes()
+  fmt.Printf("Generated Caddyfile content :\n %v \n\n", buffer.String())
+  ioutil.WriteFile("/Caddyfile", buffer.Bytes(), 644)
+}
+
+func launchCaddy() {
+  cmd := exec.Command("/caddy", "--pidfile", "/caddy.pid", "--conf", "/Caddyfile")
+  if err := cmd.Run(); err != nil {
+    if err.Error() == "exit status 1" && cmd.Process.Pid != getCaddyPid() {
+      fmt.Printf("Parent process exited with child alive")
+    } else {
+      panic(err)
+    }
+  }
+}
+
+func getCaddyPid() int {
+  filebytes, err := ioutil.ReadFile("/caddy.pid")
+  if err != nil { panic(err) }
+  pid := bytes.NewBuffer(filebytes).String()
+  intpid, err := strconv.Atoi(pid)
+  return intpid
+}
+
+func reloadCaddy() {
+  fmt.Printf("Reload caddy pid: %v", getCaddyPid())
+  syscall.Kill(getCaddyPid(), 10) 
 }
 
 func main() {
   kubeClient, err := client.NewInCluster()
   if err != nil { panic(err) }
-  router := getRouter(kubeClient)
+  regenerateCaddyfile(getRouter(kubeClient))
   watch, err := kubeClient.Extensions().Ingress(api.NamespaceAll).Watch(api.ListOptions{})
-  if err != nil { panic(err) }  
+  if err != nil { panic(err) }
+  go launchCaddy()
+  time.Sleep(time.Second)
   evts := watch.ResultChan()
   for {
     evt := <-evts
-    if caddy.IsRestart() {
-      fmt.Printf("Skip Caddy restart on evt : %v", evt)
-      os.Setenv("CADDY_RESTART", "false")
-    } else {
-      fmt.Printf("Restart Caddy due to evt : %v", evt)
-      err := caddy.Restart(caddy.CaddyfileInput{Contents: []byte(regenerateCaddyfile(router))})
-      if err != nil { panic(err) }
-//      caddy.Wait()
-      os.Exit(0)
-    }
+    fmt.Printf("Restart Caddy due to evt : %v", evt)
+    regenerateCaddyfile(getRouter(kubeClient))
+    reloadCaddy()
   }
-
 }
