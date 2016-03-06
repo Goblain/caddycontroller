8682e9f04bab740f42cb88e8914f8e7ee3a79c33
Radek 'Goblin' Pieczonka <goblin@pentex.pl>
2016-03-06 15:23:17 +0000

    makes sure to reload only on valid event, adds zombie reaper

--- controller.go
+++ controller.go
@@ -4,10 +4,13 @@ import (
   "syscall"
   "time"
   "fmt"
+  "github.com/golang/glog"
   "io/ioutil"
   "strconv"
   "bytes"
+  "os"
   "os/exec"
+  "os/signal"
   "text/template"
   "k8s.io/kubernetes/pkg/api"
   client "k8s.io/kubernetes/pkg/client/unversioned"
@@ -84,7 +87,37 @@ func reloadCaddy() {
   syscall.Kill(getCaddyPid(), 10) 
 }
 
+
+// CaddyController is intended to launch as root process of a contaier in K8S POD, 
+// aS such, it faces PID1 zombie reaping responsibility so reaping function is created below
+// (code borrowed from OpenShift)
+func grimmReaper() {
+// that has pid 1.
+  if os.Getpid() == 1 {
+    go func() {
+      sigs := make(chan os.Signal, 1)
+      signal.Notify(sigs, syscall.SIGCHLD)
+      for {
+        // Wait for a child to terminate
+        sig := <-sigs
+        glog.Infof("Signal recieved : %v", sig)
+        for {
+          // Reap processes
+          _, err := syscall.Wait4(-1, nil, 0, nil)
+          
+          // Break out if there are no more processes to read
+          if err == syscall.ECHILD {
+            break
+          }
+        }
+      }
+    }()
+  }
+}
+
+
 func main() {
+  grimmReaper()
   kubeClient, err := client.NewInCluster()
   if err != nil { panic(err) }
   regenerateCaddyfile(getRouter(kubeClient))
@@ -95,8 +128,13 @@ func main() {
   evts := watch.ResultChan()
   for {
     evt := <-evts
-    fmt.Printf("Restart Caddy due to evt : %v", evt)
-    regenerateCaddyfile(getRouter(kubeClient))
-    reloadCaddy()
+    if evt.Type == "ADDED" || evt.Type == "MODIFIED" || evt.Type == "DELETED" {
+      fmt.Printf("Restart Caddy due to evt : %v", evt)
+      regenerateCaddyfile(getRouter(kubeClient))
+      reloadCaddy()
+    } else {
+      fmt.Printf("Something went wrong - evt: %v", evt)
+      time.Sleep(time.Second)
+    }
   }
 }
